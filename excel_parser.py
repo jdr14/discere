5c0ba2a51e92144f353ba26df2adb7e964b452b6
Joseph Rodgers <jdr14@vt.edu>
2018-06-26 22:42:24 -0700

    Thoroughly tested the code added responsible for determining if there is a valid excel file in the current directory and giving the user the option to select which excel file to read if there is more than one file.  In addition, the user has the option to provide a custom path as a command line argument when running the script.

--- excel_parser.py
+++ excel_parser.py
@@ -14,6 +14,7 @@ finally:
 
 from textwrap import dedent
 import xlrd  # Only available in Python 3.X
+import time
 import os
 
 EXCEL_FILE_FORMATS = ['xls', 'xlsx', 'xlt', 'xltx']  # Different standard Excel formats
@@ -21,19 +22,22 @@ COLORS_ENABLED = True
 
 # Defined outside of the '_colorText' function for run time efficiency purposes
 ANSI_COLORS_DICT = {
-    'red'     : '\033[1;31;40m',
-    'green'   : '\033[1;32;40m',
-    'yellow'  : '\033[1;33;40m',
-    'blue'    : '\033[1;34;40m',
-    'magenta' : '\033[1;35;40m',
-    'cyan'    : '\033[1;36;40m',
-    'white'   : '\033[1;37;40m',
+    'red'     : '1;31;40m',
+    'green'   : '1;32;40m',
+    'yellow'  : '1;33;40m',
+    'blue'    : '1;34;40m',
+    'magenta' : '1;35;40m',
+    'cyan'    : '1;36;40m',
+    'white'   : '1;37;40m',
+    'escape'  : '0;0m',
 }
 
 def _colorText(msg, color):
     try:
         if COLORS_ENABLED:
-            msg = "{0}{1}{2}".format(ANSI_COLORS_DICT[color], msg, ANSI_COLORS_DICT[colors])
+            msg = "{0}{1}{2}".format('\033[{}'.format(ANSI_COLORS_DICT[color]), 
+                                     msg, 
+                                     '\033[{}'.format(ANSI_COLORS_DICT['reset']))
     except KeyError:
         # Code to log error  (TO_DO)
         pass
@@ -55,33 +59,69 @@ def _getNumExcelFilesInCWD():
     """
     _excelFileCounter = 0
     for item in os.listdir(os.getcwd()):
-        if item[-item.index('.'):] in EXCEL_FILE_FORMATS:
+        if item[item.index('.') + 1:] in EXCEL_FILE_FORMATS:
             _excelFileCounter += 1
     return _excelFileCounter
 
-def _getNumExcelFilesInCWD():
+def _getListExcelFilesInCWD():
+    """
+    Gets the number of excel files in 
+    """
     _excel_file_list = []
     for item in os.listdir(os.getcwd()):
-        if item[-item.index('.'):] in EXCEL_FILE_FORMATS:
+        if item[item.index('.') + 1:] in EXCEL_FILE_FORMATS:
             _excel_file_list.append(item)
     return _excel_file_list
 
+def _checkQuit(input):
+    """
+    Helper function to cleanly exit the program
+    """
+    if input == 'q':
+        print(_colorText(" Exiting now... Goodbye.", 'magenta'))
+        time.sleep(2)  # Pause 
+        sys.exit(0)
 
 def getExcelFile():
-    if _commandLineArgsIn() and _fileExists(sys.argv[2]):
-        return sys.argv[2]  # User provided an excel file through command line argument
-    else:
-        if _getNumExcelFilesInCWD() == 0:
-            _errMsg = """
-                Error:  No excel file found in {}"
-                Recommended:  Please move excel file to the folder you are running this parser in
-                OR specify the full path to the excel file as a command line argument.
-                """
-            print(dedent(_errMsg.format(os.getcwd())))
-            sys.exit(0)
-        elif _getNumExcelFilesInCWD() == 1:  # (TO_DO)
-            pass  
-        else:  # number of excel files in CWD > 1  (TO_DO)
-            pass
+    """
+    """
+    # User provided an excel file through command line argument
+    if _commandLineArgsIn() and _fileExists(sys.argv[1]):
+        return sys.argv[1]  
+    
+    # User did not provide a custom path to excel file.  Check CWD for valid file to parse
+    if _getNumExcelFilesInCWD() == 0:
+        _errMsg = """
+            Error:  No excel file found in {}"
+            Recommended:  Please move excel file to the folder you are running this parser in
+            OR specify the full path to the excel file as a command line argument.
+            """
+        print(_colorText(dedent(_errMsg.format(os.getcwd())), 'red'))
+        sys.exit(0)
+    elif _getNumExcelFilesInCWD() == 1:  # (TO_DO)
+        return _getListExcelFilesInCWD()[0]
+    else:  # number of excel files in CWD > 1  (TO_DO)
+        # Store the excel files in a list var to avoid reiterating (thereby improving performance)
+        file_list = _getListExcelFilesInCWD()
+        
+        # Neatly print out all of the file options for the user to choose from with corresponding numbers
+        for excel_file in file_list:
+            print(_colorText(" ( {0} )  {1}".format(file_list.index(excel_file), excel_file), 'yellow'))
+        
+        # Get user input
+        user_in = input("\n Please enter a number corresponding to the file you want to parse.\n File #> ")
+        
+        # Ensure the user input is valid
+        while not user_in.isdigit() or not int(user_in) in range(0, len(file_list)):
+            user_in = input("\n Please enter a number between 0 and {} or enter 'q' to quit.\n File #> ".format(len(file_list)))
+            
+            # Leave the user the option to cleanly exit the program
+            _checkQuit(user_in)
+        
+        # User input should be valid now once at this stage.  Use it to select the excel file
+        return file_list[int(user_in)]    
+
+print(getExcelFile())
+
 
 
